19546e2259dbd9504bf3234c6e345c1a58049ef2
Bernard Kolobara <bernard.kolobara@gmail.com>
2013-10-16 22:43:20 +0200

    Add better description to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sicp-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My solutions to SICP exercises in Clojure"
+  :url "https://github.com/bkolobara/sicp-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
